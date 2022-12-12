eaa546d0d838e5e8dddeabc2a34ff231f194f004
Taha Afzal <tahaafzal5@hotmail.com>
2022-12-11 21:00:10 -0600

    testing js stuff.

---
+++ footer.js
@@ -0,0 +1,4 @@
+export function loadFooterText() {
+    const footerText = document.getElementById('footer-text');
+    footerText.innerHTML = 'This is the footer text.';
+}
---
+++ script.js
@@ -0,0 +1,76 @@
+import { loadFooterText } from './footer.js';
+
+function body() {
+    loadLeftSide();
+    loadRightSide();
+}
+
+function loadLeftSide() {
+    loadProfileOverview();
+    loadRresume();
+}
+
+function loadProfileOverview() {
+
+}
+
+function loadRresume() {
+    loadWorkExperiences();
+    loadEducations();
+    loadHonorsAndAwards();
+    loadPersonalProjects();
+    loadLeadershipAndAffiliations();
+}
+
+function loadWorkExperiences() {
+
+}
+
+function loadEducations() {
+
+}
+
+function loadHonorsAndAwards() {
+
+}
+
+function loadPersonalProjects() {
+
+}
+
+function loadLeadershipAndAffiliations() {
+
+}
+
+function loadRightSide() {
+    loadAbout();
+    loadAdvancedSkills();
+    loadIntermediateSkills();
+    loadContactInfo();
+    loadLogos();
+    loadFooterText();
+}
+
+function loadAbout() {
+
+}
+
+function loadAdvancedSkills() {
+
+}
+
+function loadIntermediateSkills() {
+
+}
+
+function loadContactInfo() {
+
+}
+
+function loadLogos() {
+
+}
+
+function loadFooterText() {
+
+}
